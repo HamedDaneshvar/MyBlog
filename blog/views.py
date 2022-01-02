b374acba98f952b5ed40cfc8730f3adcf3e8f60f
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-02 14:43:19 +0330

    read title and navbar with template tag

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,12 @@ from .models import Article, Category
 # Create your views here.
 def home(request):
     context = {
-        'articles': Article.objects.filter(status='p'),
-        'category': Category.objects.filter(status=True)
+        'articles': Article.objects.filter(status='p')
     }
     return render(request, 'blog/home.html', context)
 
 def detail(request, slug):
     context = {
-        'article': get_object_or_404(Article, slug=slug, status='p'),
-        'category': Category.objects.filter(status=True)
+        'article': get_object_or_404(Article, slug=slug, status='p')
     }
     return render(request, 'blog/detail.html', context)
