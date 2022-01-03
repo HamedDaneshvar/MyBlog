8078475505c8d1aa4b5a88283e77faa3868750ac
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-03 21:14:20 +0330

    use ListView instead category view with little change

--- blog/views.py
+++ blog/views.py
@@ -31,13 +31,28 @@ class ArticleDetail(DetailView):
         return get_object_or_404(Article.objects.published(), slug=slug)
 
 
-def category(request, slug, page=1):
-    category = get_object_or_404(Category, slug=slug, status=True)
-    article_list = category.articles.published()
-    paginator = Paginator(article_list, 3)
-    articles = paginator.get_page(page)
-    context = {
-        'category': category,
-        'articles': articles
-    }
-    return render(request, 'blog/category.html', context)
+# def category(request, slug, page=1):
+#     category = get_object_or_404(Category, slug=slug, status=True)
+#     article_list = category.articles.published()
+#     paginator = Paginator(article_list, 3)
+#     articles = paginator.get_page(page)
+#     context = {
+#         'category': category,
+#         'articles': articles
+#     }
+#     return render(request, 'blog/category.html', context)
+class CategoryList(ListView):
+    paginate_by = 3
+    template_name = 'blog/category_list.html'
+
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category.objects.active(), slug=slug)
+        return category.articles.published()
+
+    # for editing context and add category to context and send it
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
