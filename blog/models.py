eea6dd858816ea6378019e7c5cb439e54ba433c0
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-03 11:09:48 +0330

    fix bugs in admin panel and category template with use managers

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.utils import timezone
 from extensions.utils import jalali_converter
 
+
+# my managers
+class ArticleManager(models.Manager):
+    
+    def published(self):
+        return self.filter(status='p')
+
+
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=200, verbose_name="عنوان دسته‌بندی")
@@ -57,3 +65,7 @@ class Article(models.Model):
 
     def category_published(self):
         return self.category.filter(status=True)
+
+
+    # set new manager for article
+    object = ArticleManager()
