8d0d2d79a1aba1d6dca583e432f2169dc93f3079
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-03 20:44:39 +0330

    use Listview instead by home func in view

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import ListView
 from .models import Article, Category
 
 # Create your views here.
-def home(request, page=1):
-    article_list = Article.objects.published()
-    paginator = Paginator(article_list, 6)
-    articles = paginator.get_page(page)
-    context = {
-        'articles': articles,
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request, page=1):
+#     article_list = Article.objects.published()
+#     paginator = Paginator(article_list, 6)
+#     articles = paginator.get_page(page)
+#     context = {
+#         'articles': articles,
+#     }
+#     return render(request, 'blog/home.html', context)
+class ArticleList(ListView):
+    # model = Article
+    # template_name = 'blog/home.html'
+    # context_object_name = "articles"
+    queryset = Article.objects.published()
+    paginate_by = 6
+
 
 def detail(request, slug):
     context = {
