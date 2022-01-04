b1e4c09be4407539f8fcfd114964ba681ba0c081
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-04 16:04:23 +0330

    add action for category and article in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,44 @@ from django.contrib import admin
 from .models import Article, Category
 
 # Register your models here.
+def make_published(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='p')
+    if rows_updated == 1:
+        message_bit = "منتشر شد."
+    else:
+        message_bit = "منتشر شدند."
+    modeladmin.message_user(request, f"{rows_updated} مقاله {message_bit}")
+make_published.short_description = "انتشار مقالات انتخاب شده"
+
+def make_draft(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='d')
+    if rows_updated == 1:
+        message_bit = "پیش‌نویس شد."
+    else:
+        message_bit = "پیش‌نویس شدند."
+    modeladmin.message_user(request, f"{rows_updated} مقاله {message_bit}")
+make_draft.short_description = "پیش‌نویس شدن مقالات انتخاب شده"
+
+def make_active(modeladmin, request, queryset):
+    rows_updated = queryset.update(status=True)
+    if rows_updated == 1:
+        message_bit = "نمایش داده شد."
+    else:
+        message_bit = "نمایش داده شدند."
+    modeladmin.message_user(request, f"{rows_updated} دسته‌بندی {message_bit}")
+make_active.short_description = "نمایش دسته‌بندی انتخاب شده"
+
+def make_disable(modeladmin, request, queryset):
+    rows_updated = queryset.update(status=False)
+    modeladmin.message_user(request, f"{rows_updated} دسته‌بندی غیر فعال شد.")
+make_disable.short_description = "عدم نمایش دسته‌بندی انتخاب شده"
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['position', 'title', 'slug', 'parent', 'status']
     list_filter = ['status']
     search_fields = ['title', 'slug']
+    actions = [make_active, make_disable]
 
     # create slug field from title of article
     prepopulated_fields = {'slug': ('title',)}
@@ -19,6 +53,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ['publish', 'status']
     search_fields = ['title', 'description']
     ordering = ['status', 'publish']
+    actions = [make_published, make_draft]
 
     # create slug field from title of article
     prepopulated_fields = {'slug': ('title',)}
