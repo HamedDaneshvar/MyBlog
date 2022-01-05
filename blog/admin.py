7e2be5bdd9f8ec6dfbe234143233d160ebf1f9da
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-05 09:45:48 +0330

    add User from django default model as Author

--- blog/admin.py
+++ blog/admin.py
@@ -49,8 +49,8 @@ admin.site.register(Category, CategoryAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'thumbnail_tag', 'slug', 'jpublish', 'status', 'category_to_str']
-    list_filter = ['publish', 'status']
+    list_display = ['title', 'thumbnail_tag', 'slug', 'author', 'jpublish', 'status', 'category_to_str']
+    list_filter = ['author', 'publish', 'status']
     search_fields = ['title', 'description']
     ordering = ['status', 'publish']
     actions = [make_published, make_draft]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.views.generic import ListView, DetailView
 from .models import Article, Category
@@ -55,4 +56,21 @@ class CategoryList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = category
+        return context
+
+
+class AuthorList(ListView):
+    paginate_by = 3
+    template_name = 'blog/author_list.html'
+
+    def get_queryset(self):
+        global author
+        username = self.kwargs.get('username')
+        author = get_object_or_404(User, username=username)
+        return author.articles.published()
+
+    # for editing context and add author to context and send it
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author'] = author
         return context
