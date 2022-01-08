f1287b7f1ee60c4001536bee15c4d5d8366a656c
Hamed Daneshvar <hmddaneshvar@gmail.com>
2022-01-08 10:29:54 +0330

    add password section to profile

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,8 @@ from .views import (
     ArticleDelete,
     ArticleList,
     ArticleUpdate,
-    Profile
+    PasswordChange,
+    Profile,
 )
 
 
@@ -14,8 +15,8 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
-    # path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_change/', PasswordChange.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     # path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
     # path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from .models import User
 from .forms import ProfileForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -70,3 +70,7 @@ class Login(LoginView):
             return reverse_lazy('account:home')
         else:
             return reverse_lazy('account:profile')
+
+
+class PasswordChange(PasswordChangeView):
+    success_url = reverse_lazy("account:password_change_done")
